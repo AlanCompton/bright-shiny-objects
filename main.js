f10ad0c93cab4d08ef2a5c4f7d9bfa0e66b55e54
AlanCompton <alan.compton@codeimmersives.com>
2022-10-16 06:20:24 -0500

    main.js uploaded

--- main.js
+++ main.js
@@ -2,14 +2,59 @@
 // │ Your code here! │
 // └─────────────────┘
 
+// createUser
+function createUser(firstName,lastName){
+    let user = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return user;
+}
 
+// setAge
+function setAge(user,age){
+    user.age = age;
+    return user;
+}
 
+// incrementAge
+function incrementAge(user){
+    user.age = user.age +1;
+    return user;
+}
 
+// fixCar
+function fixCar(car){
+    car.needsMaintenance = false;
+    return car;
+}
 
+// addGrades
+function addGrades(student, newGrades){
+    student.grades = student.grades.concat(newGrades);
+    return student;
+}
 
+// getDataType
+function getDataType(car,keys){
+    keyTypes = typeof car[keys];
+    return keyTypes;
+}
 
+// addToDo
+function addTodo(todos,addNewToDo){
+    todos.push(addNewToDo);
+    return todos;
+} 
 
+// addSong
+function addSong(playList,newSong){
+    playList.songs.push(newSong);
+    playList['duration']=(playList['duration']+=newSong['duration']);
+    return playList;
+}
 
+// updateReportCard
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
